cbb7f280b9fcf2d689a69d413ef79497d42ee765
M. Korchevi <marinastudenko@gmail.com>
2021-02-12 11:39:12 +0100

    Agregar: ejercicio num to string

---
+++ num-to-string/numToString.js
@@ -0,0 +1,13 @@
+// write a function which takes one argument which is a number
+// and returns the same value as a string
+
+numberToString(10) 
+// returns
+"10" 
+
+//solution
+//number.toString( [base ] )
+
+function numberToString(number) {
+  return number.toString();
+}
